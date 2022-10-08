e564dc57128c904948f69e4e794e932cc5821bb7
Ratnesh125 <87529730+Ratnesh125@users.noreply.github.com>
2022-10-09 01:02:54 +0530

    Create Linked List Basics.cpp

---
+++ Linked List Basics.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x =20;
+    int *ptr;           //initialize
+    ptr=&x;             //"&x" to store address of int x in ptr 
+    cout<<ptr<<endl;    //address
+    cout<<*ptr<<endl;   // value 
+    
+    int ** ptr1 ;       //pointer of pointer
+    ptr1 = &ptr;        //"&ptr" to store address of ptr in ptr1
+    cout<<ptr1<<endl;   //return address of ptr1  
+    cout<<*ptr1<<endl;  //return address of ptr 
+    cout<<**ptr1<<endl; //return value 
+    
+    int arr[]={2,4,5,68,5};
+    int * ptr2 ;
+    ptr2=arr;
+    //or ptr2 =&arr[0];
+    cout<<ptr2<<endl;
+    cout<<*ptr2;
+    
+    return 0;
+}
